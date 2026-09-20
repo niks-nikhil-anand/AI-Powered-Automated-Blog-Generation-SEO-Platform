751c599d4af9279c94445114032d853f2560f030
niks-nikhil-anand <niks.anand.developer@gmail.com>
2026-09-20 12:10:47 +0530

    refactor(research-worker): remove pipeline/promote.ts

--- workers/research-worker/pipeline/promote.ts
+++
@@ -1,6 +0,0 @@
-import { researchConfig } from "../config";
-import { ResearchCandidate } from "../types";
-
-export function promotableCandidates(candidates: ResearchCandidate[]): ResearchCandidate[] {
-  return candidates.filter((candidate) => candidate.score >= researchConfig.minScoreToPromote);
-}
